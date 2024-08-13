657331a52183c18c615a17b9437b283e8ee24da1
Julia Imlauer <julia.imlauer@gmail.com>
2024-08-13 18:01:17 +0200

    code example for 02_02

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Rechenoperationen mit Python
+
+# Variablen
+a = 10
+b = 5
+c = 3
+
+# Addition
+summe = a + b
+print("Summe:", summe)
+
+# Subtraktion
+differenz = a - b
+print("Differenz:", differenz)
+
+# Multiplikation
+produkt = a * b
+print("Produkt:", produkt)
+
+# Division
+quotient = a / b
+print("Quotient:", quotient)
+
+# Ganzzahlige Division
+ganzzahl_quotient = a // c
+print("Ganzzahliger Quotient:", ganzzahl_quotient)
+
+# Modulo (Rest einer Division)
+rest = a % c
+print("Rest:", rest)
+
+# Potenzierung
+potenz = 2 ** c
+print("2 hoch 3:", potenz)
+
+# Aufgabe:
+# Führen Sie eine Addition, Subtraktion, Multiplikation und Division
+# mit zwei von Ihnen gewählten Variablen durch und geben Sie die Ergebnisse aus.
---
+++ script_loesung.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Rechenoperationen mit Python
+
+# Variablen
+a = 10
+b = 5
+c = 3
+
+# Addition
+summe = a + b
+print("Summe:", summe)
+
+# Subtraktion
+differenz = a - b
+print("Differenz:", differenz)
+
+# Multiplikation
+produkt = a * b
+print("Produkt:", produkt)
+
+# Division
+quotient = a / b
+print("Quotient:", quotient)
+
+# Ganzzahlige Division
+ganzzahl_quotient = a // c
+print("Ganzzahliger Quotient:", ganzzahl_quotient)
+
+# Modulo (Rest einer Division)
+rest = a % c
+print("Rest:", rest)
+
+# Potenzierung
+potenz = 2 ** c
+print("2 hoch 3:", potenz)
+
+# Aufgabe:
+# Führen Sie eine Addition, Subtraktion, Multiplikation und Division
+# mit zwei von Ihnen gewählten Variablen durch und geben Sie die Ergebnisse aus.
+meine_variable_1 = 20
+meine_variable_2 = 2.5
+meine_addition = meine_variable_1 + meine_variable_2
+print("Addition:", meine_addition)
+meine_subtraktion = meine_variable_1 - meine_variable_2
+print("Subtraktion:", meine_subtraktion)
+meine_multiplikation = meine_variable_1 * meine_variable_2
+print("Multiplikation:", meine_multiplikation)
+meine_division = meine_variable_1 / meine_variable_2
+print("Division:", meine_division)
