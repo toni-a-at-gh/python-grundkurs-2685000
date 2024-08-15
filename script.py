1cbec7dea0eb59e3758132f82068a5d5e2df7fb5
Julia Imlauer <julia.imlauer@gmail.com>
2024-08-15 19:53:29 +0200

    adding information to argparse

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Beispiel für argparse")
+parser.add_argument("pflicht_int", help="Integer der erforderlich ist", type=int)
+parser.add_argument("--optional_int", help="Optionaler Integer", type=int)
+parser.add_argument("--optional_str", help="Optionaler String")
+parser.add_argument("--wahl", help="Meine Wahl", choices=["a", "b", "c", "d"])
+args = parser.parse_args()
+
+print("Integer:", args.pflicht_int, "- Typ:", type(args.pflicht_int))
+print("Optional Integer:", args.optional_int)
+print("Optional String:", args.optional_str, "- Type:", type(args.optional_str))
+print("Wahl:", args.wahl)
+
 # Aufgabe: Erstellen Sie ein Skript, das die Länge eines Textes analysiert und Informationen darüber liefert.
 
 # Das Skript soll folgende Funktionalität bieten:
