65bbdf7a22508080b54c9a42a68e57997c1eef4b
Julia Imlauer <julia.imlauer@gmail.com>
2024-08-17 14:35:57 +0200

    adopting content

--- script.py
+++ script.py
@@ -18,21 +18,3 @@ print(f"    Speicheradresse: {id(zahl)}")
 aendere_immutable_variable(zahl)
 print(f"Nach Funktionsaufruf (immutable): {zahl}, Speicheradresse: {id(zahl)}")
 print(f"    Speicheradresse: {id(zahl)}")
-
-
-
-def aendere_mutable_variable(liste: list):
-    """Ändert den Inhalt einer mutable Variablen."""
-    print(f"Ursprüngliche Liste in der Funktion (mutable): {liste}")
-    print(f"    Speicheradresse: {id(liste)}")
-    liste.append(100)  # Mutable Variablen wie Listen werden im selben Speicherplatz verändert
-    print(f"Geänderte Liste in der Funktion (mutable): {liste}")
-    print(f"    Speicheradresse: {id(liste)}")
-
-# Beispiel mit einer mutable Variable
-meine_liste = [1, 2, 3]
-print(f"Vor Funktionsaufruf (mutable): {meine_liste}")
-print(f"    Speicheradresse: {id(meine_liste)}")
-aendere_mutable_variable(meine_liste)
-print(f"Nach Funktionsaufruf (mutable): {meine_liste}")
-print(f"    Speicheradresse: {id(meine_liste)}")
