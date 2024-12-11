75c0f4d4b6f173862f6d0181995e183ce2849346
toni-a-at-gh <toni_a@gmx.de>
2024-12-11 14:52:23 +0000

    Added some basic math operation examples in TinosBsp

--- TinosBsp.py
+++ TinosBsp.py
@@ -1,5 +1,20 @@
 a = 1
 b = 4
 c = 6
+# Zahlen ausgeben:
+print("a = ", a, "; b = ", b, "; c = ", c)
 
+#Addition:
 print ("a + b + c = ", a+b+c)
+
+#Subtraktion:
+print ("a - b - c =", a-b-c)
+
+#Division:
+print ("a / (b + c) = ", a/(b+c))
+
+#Multiplikation:
+print ("a * b * c = ", a*b*c)
+
+#Modulo:
+print("Rest von a * b / c = ", (a*b)%c)
