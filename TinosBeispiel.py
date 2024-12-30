de61f517b6550d7bcacd7a0d4b04a0354a6b196d
toni-a-at-gh <toni_a@gmx.de>
2024-12-30 14:15:39 +0000

    Konsoleneingabe

---
+++ TinosBeispiel.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+Lieblingsfilm = input("Wie lautet Ihr Lieblingsfilm? ")
+Lieblingszahl = input("Wie lautet Ihre Lieblingszahl? ")
+print("Ihr Lieblingsfilm heißt '" + Lieblingsfilm + "'. Ihre Lieblingszahl addiert mit 14.5 lautet " + str(14.5 + float(Lieblingszahl)) + ".")
