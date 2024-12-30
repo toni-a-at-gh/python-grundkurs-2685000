bde3829fb057e501874ffd482173f2fc4bf8cffb
toni-a-at-gh <toni_a@gmx.de>
2024-12-30 09:47:49 +0000

    added my if-elif-else stuff

--- TinosBeispiel.py
+++ TinosBeispiel.py
@@ -4,7 +4,11 @@ b = 8
 c = -5.4
 
 #If-Else-Anweisungen
-if a < b:
+if a == b:
+  print("a ist gleich als b")
+elif a < b:
   print("a ist kleiner als b")
 else:
-  print ("a ist nicht kleiner als b")
+  print ("a ist größer als b")
+if a < b and a > c:
+  print ("a ist kleiner als b und größer als c")
