367a189236971f091b73e6eb7ad7e86be4c11212
Tino Amme <tino.amme@cariad.technology>
2025-01-29 08:50:48 +0100

    Verarbeitung von Argumenten wetier untersucht

--- TinosExample.py
+++ TinosExample.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-# Importieren des Package, das das Verarbeiten von Argumenten ermöglicht:
+# Importieren des Moduls argparse, das das Verarbeiten von Argumenten ermöglicht:
 import argparse
 
-# Anlegen eines parser-Objekts:
-parser = argparse.
+# Name des Skripts in eine Variable schreiben
+scriptName = "TinosExample.py"
+
+# Start des Skripts
+print("Start von " + scriptName)
+
+# Anlegen eines neuen ArgumentParser-Objekts:
+
+# Hinweis: der String in "description" wird als Hilfe angezeigt, wenn das Skript
+# mittels "-h" Argument aufgerufen wird
+parser = argparse.ArgumentParser(description="Dies ist eine einfache Taschenrechner-App")
+
+# Definieren von sog. "Pflichtparametern":
+parser.add_argument()
+
+# Einlesen der Argumente bei Skriptstart:
+args = parser.parse_args()
+
+# Ergebnis ausgeben:
+print("Ende von " + scriptName)
