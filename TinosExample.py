a0fa51f9885a4954f2bcbd70b0ef75c842d5938d
Tino Amme <tino.amme@cariad.technology>
2025-01-29 18:04:27 +0100

    Pflicht- und optionale Argumente für Skript eingeführt

--- TinosExample.py
+++ TinosExample.py
@@ -16,10 +16,44 @@ print("Start von " + scriptName)
 parser = argparse.ArgumentParser(description="Dies ist eine einfache Taschenrechner-App")
 
 # Definieren von sog. "Pflichtparametern":
-parser.add_argument()
+parser.add_argument("linkerOperator", help="Linker Operator", type=float)
+parser.add_argument("rechterOperator", help="Rechter Operator", type=float)
+parser.add_argument("Operand", help="Operand ('+', '-', '*' oder '/')", choices=["+", "-", "*","/"])
+
+# Definieren von optionalen Parametern:
+parser.add_argument("--GleichungSchreiben", help="True: Gleichung wird ausgegeben. False: Nur Ergebnis wird ausgegeben.", type=int)
 
 # Einlesen der Argumente bei Skriptstart:
 args = parser.parse_args()
 
+# Ausgeben der eingegeben Argumente:
+print("Die folgenden Argumente wurden eingegeben: " + str(args.linkerOperator) + " " + str(args.rechterOperator) + " " + str(args.Operand) + " " + str(args.GleichungSchreiben))
+
+# Berechnen und ausgeben:
+if args.Operand == "+":
+  if bool(args.GleichungSchreiben) == True:
+    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " + " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) + float (args.rechterOperator)))
+  else:
+    print("Das Ergebnis lautet " + str(float(args.linkerOperator) + float (args.rechterOperator)))
+elif args.Operand == "-":
+  if bool(args.GleichungSchreiben) == True:
+   print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " - " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) - float (args.rechterOperator)))
+  else:
+   print("Das Ergebnis lautet " + str(float(args.linkerOperator) - float (args.rechterOperator)))
+elif args.Operand == "*":
+  if bool(args.GleichungSchreiben) == True:
+    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " * " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) * float (args.rechterOperator)))
+  else:
+   print("Das Ergebnis lautet " + str(float(args.linkerOperator) * float (args.rechterOperator)))
+elif args.Operand == "/":
+   if bool(args.GleichungSchreiben) == True:
+    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " / " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) / float (args.rechterOperator)))
+   else:
+    print("Das Ergebnis lautet " + str(float(args.linkerOperator) / float (args.rechterOperator)))
+else:
+  print("Der Operator wird nicht untersützt.")
+
 # Ergebnis ausgeben:
 print("Ende von " + scriptName)
+
+# 2025-01-29: HIER WEITERMACHEN: Das optionale Argument wird nicht erkannt als bool
