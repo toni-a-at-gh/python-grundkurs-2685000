5d27b04779c2c797cded58e70f60c26ca1ee888a
Tino Amme <tino.amme@cariad.technology>
2025-03-03 11:47:04 +0100

    Fehler korrigiert

--- TinosExample.py
+++ TinosExample.py
@@ -13,45 +13,102 @@ print("Start von " + scriptName)
 
 # Hinweis: der String in "description" wird als Hilfe angezeigt, wenn das Skript
 # mittels "-h" Argument aufgerufen wird
-parser = argparse.ArgumentParser(description="Dies ist eine einfache Taschenrechner-App")
+parser = argparse.ArgumentParser(
+    description="Dies ist eine einfache Taschenrechner-App"
+)
 
 # Definieren von sog. "Pflichtparametern":
 parser.add_argument("linkerOperator", help="Linker Operator", type=float)
 parser.add_argument("rechterOperator", help="Rechter Operator", type=float)
-parser.add_argument("Operand", help="Operand ('+', '-', '*' oder '/')", choices=["+", "-", "*","/"])
+parser.add_argument(
+    "Operand", help="Operand ('+', '-', '*' oder '/')", choices=["+", "-", "*", "/"]
+)
 
 # Definieren von optionalen Parametern:
-parser.add_argument("--GleichungSchreiben", help="True: Gleichung wird ausgegeben. False: Nur Ergebnis wird ausgegeben.", type=int)
+parser.add_argument(
+    "--GleichungSchreiben",
+    help="True: Gleichung wird ausgegeben. False: Nur Ergebnis wird ausgegeben.",
+    type=bool,
+)
 
 # Einlesen der Argumente bei Skriptstart:
 args = parser.parse_args()
 
 # Ausgeben der eingegeben Argumente:
-print("Die folgenden Argumente wurden eingegeben: " + str(args.linkerOperator) + " " + str(args.rechterOperator) + " " + str(args.Operand) + " " + str(args.GleichungSchreiben))
+print(
+    "Die folgenden Argumente wurden eingegeben: "
+    + str(args.linkerOperator)
+    + " "
+    + str(args.rechterOperator)
+    + " "
+    + str(args.Operand)
+    + " "
+    + str(args.GleichungSchreiben)
+)
 
 # Berechnen und ausgeben:
 if args.Operand == "+":
-  if bool(args.GleichungSchreiben) == True:
-    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " + " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) + float (args.rechterOperator)))
-  else:
-    print("Das Ergebnis lautet " + str(float(args.linkerOperator) + float (args.rechterOperator)))
+    if bool(args.GleichungSchreiben) == True:
+        print(
+            "Das Ergebnis lautet: "
+            + str(float(args.linkerOperator))
+            + " + "
+            + str(float(args.rechterOperator))
+            + " = "
+            + str(float(args.linkerOperator) + float(args.rechterOperator))
+        )
+    else:
+        print(
+            "Das Ergebnis lautet "
+            + str(float(args.linkerOperator) + float(args.rechterOperator))
+        )
 elif args.Operand == "-":
-  if bool(args.GleichungSchreiben) == True:
-   print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " - " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) - float (args.rechterOperator)))
-  else:
-   print("Das Ergebnis lautet " + str(float(args.linkerOperator) - float (args.rechterOperator)))
+    if bool(args.GleichungSchreiben) == True:
+        print(
+            "Das Ergebnis lautet: "
+            + str(float(args.linkerOperator))
+            + " - "
+            + str(float(args.rechterOperator))
+            + " = "
+            + str(float(args.linkerOperator) - float(args.rechterOperator))
+        )
+    else:
+        print(
+            "Das Ergebnis lautet "
+            + str(float(args.linkerOperator) - float(args.rechterOperator))
+        )
 elif args.Operand == "*":
-  if bool(args.GleichungSchreiben) == True:
-    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " * " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) * float (args.rechterOperator)))
-  else:
-   print("Das Ergebnis lautet " + str(float(args.linkerOperator) * float (args.rechterOperator)))
+    if bool(args.GleichungSchreiben) == True:
+        print(
+            "Das Ergebnis lautet: "
+            + str(float(args.linkerOperator))
+            + " * "
+            + str(float(args.rechterOperator))
+            + " = "
+            + str(float(args.linkerOperator) * float(args.rechterOperator))
+        )
+    else:
+        print(
+            "Das Ergebnis lautet "
+            + str(float(args.linkerOperator) * float(args.rechterOperator))
+        )
 elif args.Operand == "/":
-   if bool(args.GleichungSchreiben) == True:
-    print("Das Ergebnis lautet: " + str(float(args.linkerOperator))+ " / " + str(float (args.rechterOperator)) + " = " + str(float(args.linkerOperator) / float (args.rechterOperator)))
-   else:
-    print("Das Ergebnis lautet " + str(float(args.linkerOperator) / float (args.rechterOperator)))
+    if bool(args.GleichungSchreiben) == True:
+        print(
+            "Das Ergebnis lautet: "
+            + str(float(args.linkerOperator))
+            + " / "
+            + str(float(args.rechterOperator))
+            + " = "
+            + str(float(args.linkerOperator) / float(args.rechterOperator))
+        )
+    else:
+        print(
+            "Das Ergebnis lautet "
+            + str(float(args.linkerOperator) / float(args.rechterOperator))
+        )
 else:
-  print("Der Operator wird nicht untersützt.")
+    print("Der Operator wird nicht untersützt.")
 
 # Ergebnis ausgeben:
 print("Ende von " + scriptName)
