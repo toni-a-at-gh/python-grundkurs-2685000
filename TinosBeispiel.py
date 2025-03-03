19466e45af1774156e708018cb6ffe97bc162e32
Tino Amme <tino.amme@cariad.technology>
2025-03-03 16:32:32 +0100

    Skript mit Funktionen begonnen

---
+++ TinosBeispiel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Definition der Funktionen:
+def Subtrahieren(op1: int, op2: int) -> int:
+    """Subtraktion von zwei Operatoren"""
+    return op1 - op2
+
+
+def Addieren(op1: int, op2: int) -> int:
+    """Addition von zwei Operatoren"""
+    return op1 + op2
+
+
+def Multiplizieren(op1: int, op2: int) -> int:
+    """Multiplikation von zwei Opertoren"""
+    return op1 * op2
+
+
+def Dividieren(op1: int, op2: int) -> int:
+    """Dividieren von zwei Operatoren"""
+    return op1 / op2
+
+
+# 2025-03-03: HIER WEITERMACHEN: Division durch 0 abfangen
